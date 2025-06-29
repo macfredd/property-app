665e12292b6a71232585fa1bb380558069c6bac5
Freddy Cruz Estrada <freddy.cruze@gmail.com>
2025-06-28 18:47:34 -0600

    Adding leftSideBar and other minor fixes

--- src/app/shared/left-sidebar/left-sidebar.ts
+++ src/app/shared/left-sidebar/left-sidebar.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-left-sidebar',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './left-sidebar.html',
   styleUrl: './left-sidebar.css'
 })
 export class LeftSidebar {
 
+  isExpanded = false;
+  contentVisible = false;
+  animationDuration = 500;
+
+  expand() {
+    this.isExpanded = true;
+    setTimeout(() => {
+      this.contentVisible = true;
+    }, this.animationDuration);
+  }
+
+  collapse() {
+    this.isExpanded = false;
+    this.contentVisible = false;
+  }
 }
