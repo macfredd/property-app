8513aedcb6a5d36f737bc791fbafa09020257a5e
Freddy Cruz Estrada <freddy.cruze@gmail.com>
2025-07-03 10:46:47 -0600

    Adding Map section to home page

---
+++ src/app/features/map-section/map-section.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-map-section',
+  imports: [CommonModule],
+  templateUrl: './map-section.html',
+  styleUrl: './map-section.css'
+})
+export class MapSection {
+
+}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { HeroSection } from '../../features/hero-section/hero-section';
 import { CategorySection } from '../../features/category-section/category-section';
 import { FeaturedSection } from '../../features/featured-section/featured-section';
+import { MapSection } from '../../features/map-section/map-section';
+
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [HeroSection, FeaturedSection, CategorySection],
+  imports: [HeroSection, FeaturedSection, CategorySection, MapSection],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
